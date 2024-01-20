24447c796aa03d628583771e7d6fb2e669d76cf5
rt21hyuk <156268464+rt21hyuk@users.noreply.github.com>
2024-01-20 21:27:41 +0900

    [Silver V] Title: 방 번호, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/1475. 방 번호/방 번호.py
@@ -0,0 +1,20 @@
+import sys
+# sys.stdin = open("input.txt", "r")
+
+cards = input()
+cardSetNum = 0
+cardNum = [0 for i in range(9)]
+
+def solve():
+    for card in cards:
+        cardIndex = int(card)
+
+        if cardIndex == 9:
+            cardIndex = 6
+
+        cardNum[cardIndex] = cardNum[cardIndex] + 1
+
+    cardNum[6] = cardNum[6] // 2 + cardNum[6] % 2
+    print(max(cardNum))
+
+solve()
