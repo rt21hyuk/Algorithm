78c2be5985a376087455c484b4d7032333f30a17
rt21hyuk <156268464+rt21hyuk@users.noreply.github.com>
2024-01-31 15:53:30 +0900

    [Gold V] Title: 집합의 표현, Time: 268 ms, Memory: 76584 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.py
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -1,39 +1,35 @@
 import sys
-sys.setrecursionlimit(10**6)
+sys.setrecursionlimit(1000000)
 input = sys.stdin.readline
+# sys.stdin = open('input.txt')
 
-N, M = map(int,input().split())
+sumCommand = 0
+printCommand = 1
+n, commandNum = map(int, input().split())
+parentList = [i for i in range(n + 1)]
 
-parent = [0] * (N+1) # 부모 테이블 생성
-for i in range(N+1): # 자기 자신을 부모로 설정
-    parent[i] = i
+def findParent(n):
+    if parentList[n] != n:
+        parentList[n] = findParent(parentList[n])
+    return parentList[n]
 
-# 루트 노드 찾는 함수
-def find(a):
-    if a == parent[a]: # 자기 자신이 루트 노드이면 a 반환
-        return a
-    p = find(parent[a]) # a의 루트 노드 찾기
-    parent[a] = p # 부모 테이블 갱신
-    return parent[a] # a의 루트 노드 반환
+def union(n1, n2):
+    n1 = findParent(n1)
+    n2 = findParent(n2)
 
-# a가 속해있는 집합과 b가 속해있는 집합을 합치는 연산
-def union(a,b):
-    a = find(a) # a의 루트 노드 찾기
-    b = find(b) # b의 루트 노드 찾기
-
-    if a == b: # a와 b의 루트 노드가 같으면 동일한 집합
-        return
-    if a < b: # a와 b의 루트 노드가 다르면 두 집합을 합치기
-        parent[b] = a
+    if n1 < n2:
+        parentList[n2] = n1
     else:
-        parent[a] = b
+        parentList[n1] = n2
 
-for _ in range(M):
-    o, a, b = map(int,input().split()) # operation, 원소, 원소
-    if o == 0: # o=0은 두 원소가 포함되어 있는 집합을 합치기
-        union(a,b)
-    elif o == 1: # 두 원소가 동일한 집합인지 판단
-        if find(a) == find(b):
-            print('YES')
+def solve():
+    for i in range(commandNum):
+        command, num1, num2 = map(int, input().split())
+        if command == sumCommand:
+            union(num1, num2)
         else:
-            print('NO')
+            if findParent(num1) == findParent(num2):
+                print("YES")
+            else:
+                print("NO")
+solve()
