32811359d5f4271a882f364235b111ca4bd8fb78
rt21hyuk <156268464+rt21hyuk@users.noreply.github.com>
2024-03-19 14:20:41 +0900

    [Silver II] Title: 지구 온난화, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/5212. 지구 온난화/지구 온난화.py
+++ 백준/Silver/5212. 지구 온난화/지구 온난화.py
@@ -14,7 +14,6 @@ def check(ci, cj):
             cnt += 1
     if cnt >= 3:
         sink.append([ci, cj])
-        # area[ci][cj] = "O"
 
 n, m = map(int, input().split())
 area = [list(input()) for _ in range(n)]
@@ -27,50 +26,28 @@ for i in range(n):
     for j in range(m):
         if area[i][j] == "X":
             check(i, j)
-        # if j == m-1 and "X" in area[i]:
-        #     newArea.append("".join(area[i]))
 
 if sink:
     for i, j in sink:
         area[i][j] = "."
 
-# for i in range(m):
-#     for j in range(len(newArea)):
-#         if newArea[j][i] == "X":
-#             s = min(i, s)
-#             e = max(i, e)
-#             break
-#
-# for i in range(len(newArea)):
-#     for j in range(s, e+1):
-#         # if newArea[i][j] == "O":
-#         #     print(".", end="")
-#         # else:
-#         print(newArea[i][j], end="")
-#     print()
+x1, y1 = m-1, n-1
+x2, y2 = 0, 0
 
-x1 = 0
-y1 = m-1
-x2 = 0
-y2 = 0
 # 지도 범위 구하기
 for i in range(n):
     if 'X' in area[i]:
-        x1 = i
-        break
-for i in range(n-1, -1, -1):
-    if 'X' in area[i]:
-        x2 = i
-        break
+        y1 = min(y1, i)
+        y2 = max(y2, i)
 
-for i in range(x1, x2+1):
+for i in range(y1, y2+1):
     for j in range(m):
         if area[i][j] == 'X':
-            y1 = min(y1, j)
-            y2 = max(y2, j)
+            x1 = min(x1, j)
+            x2 = max(x2, j)
 
 # 정답 출력하기
-for i in range(x1, x2+1):
-    for j in range(y1, y2+1):
+for i in range(y1, y2+1):
+    for j in range(x1, x2+1):
         print(area[i][j], end='')
     print()
