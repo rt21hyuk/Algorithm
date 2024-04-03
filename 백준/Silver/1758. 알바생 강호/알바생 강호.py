64adb9080d1f4aedb33ef7c5b5ef6e1688b5822a
rt21hyuk <156268464+rt21hyuk@users.noreply.github.com>
2024-04-03 16:06:21 +0900

    [Silver IV] Title: 알바생 강호, Time: 3980 ms, Memory: 34972 KB -BaekjoonHub

---
+++ 백준/Silver/1758. 알바생 강호/알바생 강호.py
@@ -0,0 +1,16 @@
+# import sys
+# sys.stdin = open("input.txt", "r")
+
+n = int(input())
+person = []
+for i in range(n):
+    person.append(int(input()))
+person.sort(reverse=True)
+
+total = 0
+
+for i in range(n):
+    if person[i] - i > 0:
+        total = total + person[i] - i
+
+print(total)
