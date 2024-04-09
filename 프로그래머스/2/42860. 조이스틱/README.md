bffa2184d834dfb9e8bbcb6c449cc28d301edd06
rt21hyuk <156268464+rt21hyuk@users.noreply.github.com>
2024-04-09 17:09:33 +0900

    [level 2] Title: 조이스틱, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/README.md
+++ 프로그래머스/2/42860. 조이스틱/README.md
@@ -1,10 +1,10 @@
 # [level 2] 조이스틱 - 42860 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860#) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.05 ms
+메모리: 10.2 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 09일 17:00:41
+2024년 04월 09일 17:09:33
 
 ### 문제 설명
 
--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,24 +1,17 @@
+# alpha = {chr(i): i-65 for i in range(65, 91)}
+# print(alpha)
+
 def solution(name):
-	# 조이스틱 조작 횟수 
     answer = 0
-    
-    # 기본 최소 좌우이동 횟수는 길이 - 1
-    min_move = len(name) - 1
-    
+    minMove = len(name) - 1
     for i, char in enumerate(name):
-    	# 해당 알파벳 변경 최솟값 추가
         answer += min(ord(char) - ord('A'), ord('Z') - ord(char) + 1)
         
-        # 해당 알파벳 다음부터 연속된 A 문자열 찾기
         next = i + 1
         while next < len(name) and name[next] == 'A':
             next += 1
-        print(next, end =" | ")
-        # 기존, 연속된 A의 왼쪽시작 방식, 연속된 A의 오른쪽시작 방식 비교 및 갱신
-        print(min_move,  2 *i + len(name) - next, i + 2 * (len(name) -next))
         
-        min_move = min([min_move, 2 *i + len(name) - next, i + 2 * (len(name) -next)])
-    # 알파벳 변경(상하이동) 횟수에 좌우이동 횟수 추가
-    answer += min_move
+        minMove = min(minMove, 2*i + len(name) - next, i + 2*(len(name) - next))
+            
+    answer += minMove
     return answer
-# https://velog.io/@jqdjhy/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%ED%8C%8C%EC%9D%B4%EC%8D%AC-%EC%A1%B0%EC%9D%B4%EC%8A%A4%ED%8B%B1-Greedy
