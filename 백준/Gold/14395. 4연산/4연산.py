f00edbcbf478e40e019b170a1cdd5311feca4881
rt21hyuk <156268464+rt21hyuk@users.noreply.github.com>
2024-04-09 14:54:36 +0900

    [Gold IV] Title: 4연산, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/14395. 4연산/4연산.py
+++ 백준/Gold/14395. 4연산/4연산.py
@@ -1,40 +1,30 @@
-def dfs(S,T,string,cnt):
+def dfs(S, T, string):
     global result
-    if len(string) > len(result) and result:
+    # print(S, string)
+    if result and len(string) >= len(result):
         return
-    # print(S,string,result)
     if S == T:
         if not result:
             result = string
-        else:
-            if len(string) < len(result):
-                result = string
-            elif len(string) == len(result):
-                if string < result:
-                    result = string
+        elif len(string) < len(result):
+            result = string
         return
 
-    for w in '*+/':
-        if w == '*' and S*S <= T and S > 1:
-            dfs(S*S,T,string+w,cnt)
-        elif w == '+' and S+S <= T:
-            dfs(S+S,T,string+w,cnt)
-        elif w == '/' and cnt == 0:
-            dfs(S//S,T,string+w,cnt+1)
+    if S * S <= T and S > 1:
+        dfs(S*S, T, string + '*')
+    if S + S <= T:
+        dfs(S+S, T, string + '+')
+    if not string:
+        dfs(S//S, T, string + '/')
 
-
-s, t = map(int,input().split())
+S, T = map(int,input().split())
 result = ''
-if s == t:
+if S == T :
     print(0)
-elif s == 0:
-    print(-1)
-elif t == 1:
+elif T == 1:
     print('/')
-elif t == 0:
-    print('-')
 else:
-    dfs(s,t,'',0)
+    dfs(S, T, '')
     if result:
         print(result)
     else:
