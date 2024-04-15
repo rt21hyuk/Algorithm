1bc43848599214ca4c2ebe98864d9023c17abce8
rt21hyuk <156268464+rt21hyuk@users.noreply.github.com>
2024-04-15 17:54:08 +0900

    [Gold III] Title: 파티, Time: 1356 ms, Memory: 34212 KB -BaekjoonHub

---
+++ 백준/Gold/1238. 파티/파티.py
@@ -0,0 +1,36 @@
+# import sys
+# sys.stdin = open("input.txt", "r")
+
+import heapq
+
+n, m, x = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+
+def dijkstra(start, end):
+  minCost = [float('inf') for _ in range(n+1)]
+  minCost[start] = 0
+
+  q = []
+  heapq.heappush(q, [0, start])
+
+  while q:
+    cost, cur = heapq.heappop(q)
+
+    if cur == end:
+      return cost
+
+    for next, nextCost in graph[cur]:
+      if cost + nextCost < minCost[next]:
+        minCost[next] = cost + nextCost
+        heapq.heappush(q, [minCost[next], next])
+
+for _ in range(m):
+  start, end, time = map(int, input().split())
+  graph[start].append([end, time])
+
+
+maxCost = 0
+for cur in range(1, n+1):
+  maxCost = max(dijkstra(cur, x) + dijkstra(x, cur), maxCost)
+
+print(maxCost)
