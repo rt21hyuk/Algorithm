1ca529751ffea015f51f7fe88496c634e0a925b0
rt21hyuk <156268464+rt21hyuk@users.noreply.github.com>
2024-10-10 23:16:32 +0900

    [Gold III] Title: 오등큰수, Time: 496 ms, Memory: 260996 KB -BaekjoonHub

--- 백준/Gold/17299. 오등큰수/오등큰수.py
+++ 백준/Gold/17299. 오등큰수/오등큰수.py
@@ -11,22 +11,21 @@ for num in numList:
     cntList[num] += 1
 
 stack = []
-NGFList = []
+NGFList = [-1] * n  # 결과 리스트를 미리 -1로 초기화
 
-for num in numList[::-1]:
-    if not stack:
-        NGFList.append(-1)
-    else:
-        while 1:
-            if stack:
-                if cntList[num] < cntList[stack[-1]]:
-                    NGFList.append(stack[-1])
-                    break
-                else:
-                    stack.pop()
-            else:
-                NGFList.append(-1)
-                break
+# 역순으로 탐색하며 오등큰수를 찾음
+for i in range(n - 1, -1, -1):
+    num = numList[i]
+
+    # 스택이 비어있지 않고 현재 숫자의 등장횟수가 스택의 top보다 크거나 같으면 pop
+    while stack and cntList[num] >= cntList[stack[-1]]:
+        stack.pop()
+
+    # 스택이 비어있지 않으면 해당 숫자는 오등큰수
+    if stack:
+        NGFList[i] = stack[-1]
+
+    # 스택에 현재 숫자를 추가
     stack.append(num)
 
-print(*NGFList[::-1])
+print(*NGFList)
